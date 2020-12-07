7d49e75344f7dae3482b3c2ecad9f6ff358083e6
DiegoOrtego <diego.ortegoh@gmail.com>
2020-12-07 12:44:09 +0000

    Add files via upload

---
+++ cifar10/RunScripts_cifar_MOIT.sh
@@ -0,0 +1,16 @@
+
+
+python3 train_MOIT.py --epoch 250 --num_classes 10 --batch_size 128 --low_dim 128 --M 125 --M 200 --noise_ratio 0.4 \
+--network "PR18" --lr 0.1 --wd 1e-4 --dataset "CIFAR-10" --method "MOIT" --download True --noise_type "asymmetric" \
+--batch_t 0.1 --headType "Linear" --mix_labels 1  --xbm_use 1 --xbm_begin 3 --xbm_per_class 100 \
+--balance_crit "median" --discrepancy_corrected 1 --validation_exp 0 \
+--startLabelCorrection 130 --PredictiveCorrection 1 --k_val 250 \
+--experiment_name Test_MOIT_asymmetric --cuda_dev 0
+
+
+python3 train_MOIT.py --epoch 250 --num_classes 10 --batch_size 128 --low_dim 128 --M 125 --M 200 --noise_ratio 0.4 \
+--network "PR18" --lr 0.1 --wd 1e-4 --dataset "CIFAR-10" --method "MOIT" --download True --noise_type "symmetric" \
+--batch_t 0.1 --headType "Linear" --mix_labels 1  --xbm_use 1 --xbm_begin 3 --xbm_per_class 100 \
+--balance_crit "median" --discrepancy_corrected 1 --validation_exp 0 \
+--startLabelCorrection 130 --PredictiveCorrection 1 --k_val 250 \
+--experiment_name Test_MOIT_symmetric --cuda_dev 0
